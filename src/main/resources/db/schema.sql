f7d8ba5ccb3105046d7931ca92c78ea7ad2aaa33
Kubra Ozer Temel <kubra.temel@scalefocus.com>
2022-12-14 10:31:57 +0300

    Refactoring and fixing for Postgre script

--- src/main/resources/db/schema.sql
+++ src/main/resources/db/schema.sql
@@ -1,10 +1,13 @@
+DROP TABLE IF EXISTS post;
+DROP TABLE IF EXISTS tag;
+DROP TABLE IF EXISTS post_tag;
+
 CREATE TABLE post
 (
     id           SERIAL      NOT NULL ,
     title        VARCHAR(128) NOT NULL,
     summary      VARCHAR(128),
     post_text    VARCHAR(256) NOT NULL,
-    tag          VARCHAR(64),
     created_date TIMESTAMP WITH TIME ZONE     NOT NULL,
     updated_date TIMESTAMP WITH TIME ZONE      NOT NULL,
     PRIMARY KEY (id)
@@ -21,7 +24,7 @@ CREATE TABLE tag
 CREATE TABLE post_tag
 (
     post_id INTEGER      NOT NULL,
-    tag_id  VARCHAR(128) NOT NULL,
+    tag_id  INTEGER NOT NULL,
 
     PRIMARY KEY (post_id, tag_id)
 );
